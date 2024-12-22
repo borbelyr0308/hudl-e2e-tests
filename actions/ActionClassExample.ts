c463be240f8d17ad9042d6ba7f20589f8c803c08
Robert Borbely <robert.borbely@taina.tech>
2024-12-22 23:10:40 +0000

    Added actions class example

---
+++ actions/ActionClassExample.ts
@@ -0,0 +1,22 @@
+import { Page, expect } from "@playwright/test";
+
+/**
+ * An example of an alternative to using POM, use actions classes instead.
+ * These better mimic user goals/actions, and require less code than Page 
+ * Objects, meaning less maintenance - simply use the IDE find/replace to
+ * update any locators.
+ * 
+ * @author Robert Borbely
+ */
+export class ActionClassExample {
+
+    async gotoHudleAcceptCookies(page: Page) {
+            await page.goto(process.env.HOMEPAGE || '');
+            await expect(page.getByRole('button', { name: 'Cookies Settings' })).toBeVisible();
+            await expect(page.getByRole('button', { name: 'Reject All' })).toBeVisible();
+            await expect(page.getByRole('button', { name: 'Accept All Cookies' })).toBeVisible();
+            await page.getByRole('button', { name: 'Accept All Cookies' }).click();
+            await expect(page.getByRole('link', { name: 'Allow All' })).toBeVisible();
+            await page.getByRole('link', { name: 'Allow All' }).click();
+        }
+}
--- tests/auth/login.spec.ts
+++ tests/auth/login.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { HudleHomePage } from '../../pages/home/hudleHomePage';
 import { HudleLoginPage } from '../../pages/login/hudleLoginPage';
+// import { ActionClassExample } from '../../actions/ActionClassExample'; // if you want to use actions classes uncomment this
 
 /**
  * Test to verify the Hudle Login User flow
@@ -12,7 +13,11 @@ test.describe('Authentication tests', () => {
         const hudleHomePage = new HudleHomePage(page);
         const hudleLoginPage = new HudleLoginPage(page);
 
+        // e.g. alternative to using POM is to use actions, here is an example (saves on maintenance due to less code)
+        // const actionClassExample = new ActionClassExample();
+        // await actionClassExample.gotoHudleAcceptCookies(page);
         await hudleHomePage.gotoHudleAcceptCookies(page);
+        
         await hudleHomePage.verifyLoginPageComponents();
         await hudleHomePage.gotoLogin();
 
@@ -38,8 +43,14 @@ test.describe('Authentication tests', () => {
     });
 
     test('should show errors if email or password is blank', { tag: ['@auth', '@critical'] }, async ({ page }) => {
+        // Leave email blank - verify error
+        // Fill email
+        // Leave password blank - verify error
     });
 
     test('should allow a customer to create an account', { tag: ['@auth', '@critical', '@happy'] }, async ({ page }) => {
+        // Use random-email to create different emails each run
+        // Re-use the password in .env for a valid password
+        // Verify you can create account
     });
 });
